a1cb42efcf660447fef7ba21d13a81c17c5cf521
YANFELIPEBOTELHODASI <felipeyan261@gmail.com>
2026-08-26 20:47:48 -0300

    Atualização e organização main.

--- cinePrisma/src/Main.java
+++ cinePrisma/src/Main.java
@@ -17,28 +17,7 @@ public class Main {
         scan.nextLine();
 
         if (menu == 1){
-            System.out.println("AQUI EXIBIR SESSÕES");
-
-            System.out.println("-".repeat(11) + " CinePrisma " + "-".repeat(11));
-
-            System.out.println("-".repeat(6) + " Cadastro CinePrisma " + "-".repeat(6));
-
-            System.out.println("Digite seu nome: ");
-            String cadastro = scan.nextLine();
-
-            System.out.println("Digite Sua idade: ");
-            int idade = scan.nextInt();
-            scan.nextLine();
-
-            System.out.println("Digite seu CPF: ");
-            String cpf = scan.nextLine();
-            //String somenteNumeros = cpf.replaceAll("[.\\-\\s]","");
-
-            System.out.println("----- Dados Informados -----");
-            System.out.println("Nome: "+cadastro);
-            System.out.println("Idade: "+idade);
-            System.out.println("CPF: "+cpf);
-            System.out.println("Saido do programa...");
+            filmesEmCartaz.exibirfilmes(scan);
         }
 
         else if (menu == 2){
